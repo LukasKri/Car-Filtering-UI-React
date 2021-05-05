0003a49d05fb51d22d3db6addfb1e82fbafba81e
Lukas Krisikaitis <lukaskrisikaitis@gmail.com>
2021-05-05 21:55:57 +0300

    Added car filters CSS, but not quite finnished.

--- src/App.js
+++ src/App.js
@@ -1,27 +1,28 @@
-import "./App.css";
 import CarButton from "./CarButton";
 
 function App() {
     return (
         <div className="App">
-            <h1>UI for cars category filter</h1>
-            <CarButton
-                imgAddress="https://content.r9cdn.net/rimg/carimages/generic/01_mini_white.png?width=108&height=72"
-                carType="Small"
-            />
-            <CarButton
-                imgAddress="https://content.r9cdn.net/rimg/carimages/generic/02_economy_white.png?width=108&height=72"
-                carType="Medium"
-            />
-            <CarButton
-                imgAddress="https://content.r9cdn.net/rimg/carimages/generic/03_standard_white.png?width=108&height=72"
-                carType="Large"
-            />
-            <CarButton
-                imgAddress="https://content.r9cdn.net/rimg/carimages/generic/05_suv-small_white.png?width=108&height=72"
-                carType="SUV"
-            />
-            <button className="more">More</button>
+            <div className="header">Car type</div>
+            <div className="car-filters">
+                <CarButton
+                    imgAddress="https://content.r9cdn.net/rimg/carimages/generic/01_mini_white.png?width=108&height=72"
+                    carType="Small"
+                />
+                <CarButton
+                    imgAddress="https://content.r9cdn.net/rimg/carimages/generic/02_economy_white.png?width=108&height=72"
+                    carType="Medium"
+                />
+                <CarButton
+                    imgAddress="https://content.r9cdn.net/rimg/carimages/generic/03_standard_white.png?width=108&height=72"
+                    carType="Large"
+                />
+                <CarButton
+                    imgAddress="https://content.r9cdn.net/rimg/carimages/generic/05_suv-small_white.png?width=108&height=72"
+                    carType="SUV"
+                />
+                <div className="car-filter">More</div>
+            </div>
         </div>
     );
 }
--- src/CarButton.js
+++ src/CarButton.js
@@ -1,9 +1,15 @@
 import React from "react";
 
 const CarButton = ({ imgAddress, carType }) => {
+    // const [state, setstate] = useState(false);
+
+    const handleClick = (e) => {
+        console.log("This div was clicked.");
+    };
+
     return (
-        <div className="car-button">
-            <img src={imgAddress} alt="small-car" />
+        <div className="car-filter" onClick={handleClick}>
+            <img className="car-img" src={imgAddress} alt={carType} />
             <div className="car-type">{carType}</div>
         </div>
     );
