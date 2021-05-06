d8db1c2f9c6d7f95b9c61883b157b7bfdb894547
Lukas Krisikaitis <lukaskrisikaitis@gmail.com>
2021-05-06 20:53:39 +0300

    Added isClicked state to more filter as well (toggles the color of the filter to dark and white).

--- src/MoreFilter.js
+++ src/MoreFilter.js
@@ -9,42 +9,28 @@ const MoreFilter = () => {
 
     return (
         <>
-            {isClicked ? (
-                <div
-                    className="more-filter"
-                    onClick={handleClick}
-                    style={{ background: "#2c3439", color: "#fff" }}
+            <div
+                className="more-filter"
+                onClick={handleClick}
+                style={
+                    isClicked
+                        ? { background: "#2c3439", color: "#fff" }
+                        : { background: "#fff", color: "#192024" }
+                }
+            >
+                <div>More</div>
+                <svg
+                    className="svg-image"
+                    role="img"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 200 200"
                 >
-                    <div>More</div>
-                    <svg
-                        className="svg-image"
-                        role="img"
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 200 200"
-                    >
-                        <path
-                            d="M100 132.5c-3.873 0 .136 2.376-64.801-51.738l9.603-11.523L100 115.237l55.199-45.999l9.603 11.523C99.806 134.924 103.855 132.5 100 132.5z"
-                            fill="#fff"
-                        ></path>
-                    </svg>
-                </div>
-            ) : (
-                <div
-                    className="more-filter"
-                    onClick={handleClick}
-                    style={{ background: "#fff", color: "#192024" }}
-                >
-                    <div>More</div>
-                    <svg
-                        className="svg-image"
-                        role="img"
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 200 200"
-                    >
-                        <path d="M100 132.5c-3.873 0 .136 2.376-64.801-51.738l9.603-11.523L100 115.237l55.199-45.999l9.603 11.523C99.806 134.924 103.855 132.5 100 132.5z"></path>
-                    </svg>
-                </div>
-            )}
+                    <path
+                        d="M100 132.5c-3.873 0 .136 2.376-64.801-51.738l9.603-11.523L100 115.237l55.199-45.999l9.603 11.523C99.806 134.924 103.855 132.5 100 132.5z"
+                        fill={isClicked ? "#fff" : "#192024"}
+                    ></path>
+                </svg>
+            </div>
         </>
     );
 };
