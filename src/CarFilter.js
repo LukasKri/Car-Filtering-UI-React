9b71adfafb3622ebd9976af49d24a9cf59e3162f
Lukas Krisikaitis <lukaskrisikaitis@gmail.com>
2021-05-06 20:50:20 +0300

    Added isClicked state, which toggles the color of the filter to dark and white.

--- src/CarFilter.js
+++ src/CarFilter.js
@@ -1,17 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
 const CarFilter = ({ imgAddress, carType }) => {
-    // const [state, setstate] = useState(false);
+    const [isClicked, setIsClicked] = useState(false);
 
     const handleClick = (e) => {
         console.log("Car filter was clicked.");
+        setIsClicked((prevClicked) => !prevClicked);
     };
 
     return (
-        <div className="car-filter" onClick={handleClick}>
-            <img className="car-img" src={imgAddress} alt={carType} />
-            <div className="car-type">{carType}</div>
-        </div>
+        <>
+            <div
+                className="car-filter"
+                onClick={handleClick}
+                style={
+                    isClicked
+                        ? { background: "#2c3439", color: "#fff" }
+                        : { background: "#fff", color: "#192024" }
+                }
+            >
+                <img className="car-img" src={imgAddress} alt={carType} />
+                <div className="car-type">{carType}</div>
+            </div>
+        </>
     );
 };
 
--- src/MoreFilter.js
+++ src/MoreFilter.js
@@ -1,22 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 
 const MoreFilter = () => {
+    const [isClicked, setIsClicked] = useState(false);
     const handleClick = (e) => {
         console.log("More filter was clicked.");
+        setIsClicked((prevClicked) => !prevClicked);
     };
 
     return (
-        <div className="more-filter" onClick={handleClick}>
-            <div>More</div>
-            <svg
-                className="svg-image"
-                role="img"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 200 200"
-            >
-                <path d="M100 132.5c-3.873 0 .136 2.376-64.801-51.738l9.603-11.523L100 115.237l55.199-45.999l9.603 11.523C99.806 134.924 103.855 132.5 100 132.5z"></path>
-            </svg>
-        </div>
+        <>
+            {isClicked ? (
+                <div
+                    className="more-filter"
+                    onClick={handleClick}
+                    style={{ background: "#2c3439", color: "#fff" }}
+                >
+                    <div>More</div>
+                    <svg
+                        className="svg-image"
+                        role="img"
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 200 200"
+                    >
+                        <path
+                            d="M100 132.5c-3.873 0 .136 2.376-64.801-51.738l9.603-11.523L100 115.237l55.199-45.999l9.603 11.523C99.806 134.924 103.855 132.5 100 132.5z"
+                            fill="#fff"
+                        ></path>
+                    </svg>
+                </div>
+            ) : (
+                <div
+                    className="more-filter"
+                    onClick={handleClick}
+                    style={{ background: "#fff", color: "#192024" }}
+                >
+                    <div>More</div>
+                    <svg
+                        className="svg-image"
+                        role="img"
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 200 200"
+                    >
+                        <path d="M100 132.5c-3.873 0 .136 2.376-64.801-51.738l9.603-11.523L100 115.237l55.199-45.999l9.603 11.523C99.806 134.924 103.855 132.5 100 132.5z"></path>
+                    </svg>
+                </div>
+            )}
+        </>
     );
 };
 
