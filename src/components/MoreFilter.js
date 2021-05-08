1529dfc28c6ed21a03c87a1c9c3206bdb67f687c
Lukas Krisikaitis <lukaskrisikaitis@gmail.com>
2021-05-08 14:16:28 +0300

    Made svg image rotate 180 deg on click event

--- src/components/MoreFilter.js
+++ src/components/MoreFilter.js
@@ -66,6 +66,11 @@ const MoreFilter = (props) => {
                     role="img"
                     xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 0 200 200"
+                    style={
+                        isMoreFilterClicked
+                            ? { transform: "rotate(180deg)" }
+                            : { transform: "rotate(0deg)" }
+                    }
                 >
                     <path
                         d="M100 132.5c-3.873 0 .136 2.376-64.801-51.738l9.603-11.523L100 115.237l55.199-45.999l9.603 11.523C99.806 134.924 103.855 132.5 100 132.5z"
