bebd1b0f8ea9b222903f238d348b06f2c50fe3c2
Lukas Krisikaitis <lukaskrisikaitis@gmail.com>
2021-05-08 14:18:33 +0300

    Svg image rotate refactored.

--- src/components/MoreFilter.js
+++ src/components/MoreFilter.js
@@ -24,6 +24,14 @@ const MoreFilter = (props) => {
         color: "#192024",
     };
 
+    const rotate = {
+        transform: "rotate(180deg)",
+    };
+
+    const notRotate = {
+        transform: "rotate(0deg)",
+    };
+
     const handleMoreFilterClick = (e) => {
         console.log("More filter was clicked.");
         setIsMoreFilterClicked((prevClicked) => !prevClicked);
@@ -66,11 +74,7 @@ const MoreFilter = (props) => {
                     role="img"
                     xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 0 200 200"
-                    style={
-                        isMoreFilterClicked
-                            ? { transform: "rotate(180deg)" }
-                            : { transform: "rotate(0deg)" }
-                    }
+                    style={isMoreFilterClicked ? rotate : notRotate}
                 >
                     <path
                         d="M100 132.5c-3.873 0 .136 2.376-64.801-51.738l9.603-11.523L100 115.237l55.199-45.999l9.603 11.523C99.806 134.924 103.855 132.5 100 132.5z"
